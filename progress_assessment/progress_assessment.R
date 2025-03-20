ea018db1abfcba4225bee1d29902afdabfbeb0b7
Sebastian Palmas <palmasforest@gmail.com>
2025-03-20 14:57:59 +0100

    first complete run of progress assessment

--- progress_assessment/progress_assessment.R
+++ progress_assessment/progress_assessment.R
@@ -14,32 +14,30 @@ indicator_data_MENARO <- indicator_data_WORLD |> filter(iso3 %in% MENARO_metadat
 
 # PREDICTION YEARS ----
 base.year <- 2015
-current.year <- 2024
+current.year <- 2025
 target.year <- 2030
 
 # PROGRESS ASSESSMENT ----
 # The calculations are done with a loop for each indicator and each country
 
 ## Empty table to store results ----
-country_results <- tibble(iso3 = character(0),
+progress_results <- tibble(iso3 = character(0),
                           MENARO.indicator.code = character(0),
                           points = integer(0),
                           CS = numeric(0),
-                          #CS.score = character(0),
+                          CS.score = character(0),
                           AP = numeric(0),
                           AP.score = character(0))
 
 ## Assign score loop ----
 for (i in 1:nrow(CR_SDG_indicators)){
-  #i <- 12  #to test
+  #i <- 44  #to test
   TV.i <- CR_SDG_indicators$target[i]   #final target
   direction.i <- CR_SDG_indicators$direction[i]   #direction of indicator
   denominator <- CR_SDG_indicators$denominator[i]   #denominator to use to transform to 0-1
   
   #indicator values
-  data.i <- indicator_data_MENARO |> 
-    filter(MENARO.indicator.code == CR_SDG_indicators$MENARO.indicator.code[i]) |> #data for indicator
-    mutate(obs.value.log = log(obs.value/denominator))
+  data.i <- indicator_data_MENARO |> filter(MENARO.indicator.code == CR_SDG_indicators$MENARO.indicator.code[i]) #data for indicator
   
   #count how many countries have more than 2 points for the indicator
   ctry.2plus.points <- sum(table(data.i$iso3)>1)
@@ -50,10 +48,8 @@ for (i in 1:nrow(CR_SDG_indicators)){
   
   ### Assign indicator|country score loop ----  
   for (c in MENARO_metadata$iso3){
-    #c <- 'TUN' #to test  
-    data.i.c <- data.i |>
-      filter(iso3 == c) |>   #data for indicator|country
-      arrange(time.period)  #ordering by year to properly assign value in previous measurement
+    #c <- 'QAT' #to test  
+    data.i.c <- data.i |> filter(iso3 == c)   #data for indicator|country
     
     #How many points are in this indicator|country?
     points.i.c <- nrow(data.i.c)
@@ -64,7 +60,7 @@ for (i in 1:nrow(CR_SDG_indicators)){
       base.year.pos <- which(data.i.c$time.period == base.year)
       if(identical(base.year.pos, integer(0))){
         #Estimate base year value when not found
-        obs.value.base.year <- denominator * exp(wgm(data.i.c, year.to.predict = base.year))
+        obs.value.base.year <- cagr(data = data.i.c, year.to.predict = base.year, scale = denominator)
       } else {
         #Use base year value if found
         obs.value.base.year <- data.i.c$obs.value[base.year.pos]
@@ -74,57 +70,57 @@ for (i in 1:nrow(CR_SDG_indicators)){
       current.year.pos <- which(data.i.c$time.period == current.year)
       if(identical(current.year.pos, integer(0))){
         #Estimate base year value when not found
-        obs.value.current.year <- denominator * exp(wgm(data.i.c, year.to.predict = current.year))
+        obs.value.current.year <- cagr(data = data.i.c, year.to.predict = current.year, scale = denominator)
       } else {
         #Use base year value if found
         obs.value.current.year <- data.i.c$obs.value[current.year.pos]
       }
       
       ##### Target year value----
-      obs.value.target.year <- denominator * exp(wgm(data.i.c, year.to.predict = target.year))
+      obs.value.target.year <- cagr(data = data.i.c, year.to.predict = target.year, scale = denominator)
       
       ##### Current Status ----
       CS.i.c <- CS(I_cv = obs.value.current.year, 
                    I0 = obs.value.base.year, 
                    TV = TV.i, 
                    direction = direction.i)
-      # CS.score.i.c = ifelse(CS.i.c > 9, "On-track",
-      #                       ifelse(CS.i.c <= 0, 'Regression', 'Slow progress'))
+      CS.score.i.c = ifelse(CS.i.c > 9, "On-track",
+                           ifelse(CS.i.c <= 0, 'Regression', 'Slow progress'))
       
       ##### Anticipated Progress ----
       AP.i.c <- CS(I_cv = obs.value.target.year, 
                    I0 = obs.value.base.year, 
                    TV = TV.i, 
                    direction = direction.i)
-      AP.score.i.c = ifelse(AP.i.c > 9, "On-track",
-                            ifelse(AP.i.c <= 0, 'Regression', 'Slow progress'))
+      AP.score.i.c = ifelse(AP.i.c > 9, "Will meet target",
+                            ifelse(AP.i.c <= 0, 'No progress expected', 'Need to accelerate'))
       
     } else if(points.i.c== 1){
       #### 1 data point score ----
         reached.target <- ifelse(direction.i == 'increasing',
-                                 data.i.c$obs.value >= TV,
-                                 data.i.c$obs.value <= TV)
+                                 data.i.c$obs.value >= TV.i,
+                                 data.i.c$obs.value <= TV.i)
         
         CS.i.c <- ifelse(reached.target, 10, NA)
-        #CS.score.i.c <- ifelse(reached.target, "On-track", 'Insufficient data')
+        CS.score.i.c <- ifelse(reached.target, "On-track", 'Insufficient data')
         AP.i.c. <- ifelse(reached.target, 10, NA)
-        AP.score.i.c <- ifelse(reached.target, 'On-track', 'Insufficient data')
+        AP.score.i.c <- ifelse(reached.target, 'Will meet target', 'Insufficient data')
       
     } else if (points.i.c == 0){
       #### No data points score ----
       CS.i.c <- NA
-      #CS.score.i.c <- "No data"
+      CS.score.i.c <- "No data"
       AP.i.c <- NA
       AP.score.i.c <- "No data"
     }
   
     #storing indicator|country results in final table
-    country_results <- bind_rows(country_results,
-                                 tibble(iso3 = country,
+    progress_results <- bind_rows(progress_results,
+                                 tibble(iso3 = c,
                                         MENARO.indicator.code = CR_SDG_indicators$MENARO.indicator.code[i],
                                         points = points.i.c,
                                         CS = CS.i.c,
-                                        #CS.score = CS.score.i.c,
+                                        CS.score = CS.score.i.c,
                                         AP = AP.i.c,
                                         AP.score = AP.score.i.c)) 
   }
@@ -133,3 +129,4 @@ for (i in 1:nrow(CR_SDG_indicators)){
 }
 
 # EXPORTING RESULTS ----
+save(progress_results, file = "output/progress.results.Rdata")
--- progress_assessment/progress_assessment_functions.R
+++ progress_assessment/progress_assessment_functions.R
@@ -9,8 +9,7 @@
 # I_0: base year's value
 # TV: target value
 # direction: direction of indicator (increasing or decreasing)
-# parity: if indicator is a parity indicator
-CS <- function(I_cv, I0, TV, direction = 'increasing', parity=FALSE){
+CS <- function(I_cv, I0, TV, direction){
   
   # If current value already reached or exceeded the target value, CS is 10
   if(direction == 'decreasing'){
@@ -25,19 +24,26 @@ CS <- function(I_cv, I0, TV, direction = 'increasing', parity=FALSE){
   
   # if current value has not reached target, compute exact CS value
   D <- ifelse(direction == 'decreasing', -10, 10)
+  return(D * (I_cv - I0) / abs(TV - I0))
+}
 
-  if (!parity){
-    return(D * (I_cv - I0) / abs(TV - I0))
-  } else {
-    return(10 - 10 * abs(TV - I_cv) / abs(TV - I0))
-  }
+# Geometric mean ----
+#based on geometric growth method
+gm <- function(data, year.to.predict){
+  #data <- data.i.c #to test
+  #year.to.predict <- 2030  #to test
+  data <- data |> arrange(time.period)  #ordering by year to properly assign value in previous measurement
+  T <- max(data$time.period) - min(data$time.period)
+  a <- year.to.predict - max(data$time.period)
+  return(data$obs.value[nrow(data)] * (data$obs.value[nrow(data)]/data$obs.value[1]) ^ (a/T))
 }
 
 # Weighted geometric mean ----
 wgm <- function(data, year.to.predict){
   #data <- data.i.c #to test
   #year.to.predict <- 2015  #to test
-  data <- data |> filter(time.period < year.to.predict)
+  data <- data |> arrange(time.period)  #ordering by year to properly assign value in previous measurement
+  
   year.min = min(data$time.period)
   year.max = max(data$time.period)
   data <- data |> mutate(wi = (year.to.predict - year.min)/(year.to.predict - time.period),
@@ -50,6 +56,86 @@ wgm <- function(data, year.to.predict){
   
   return(In * (prod(data$ratio.ti.im1[2:nrow(data)]))^(a/W))
   
+}
+
+
+# log regression ----
+reg <- function(data, years.to.predict, scale){
+  #data <- data.i.c #to test
+  #years.to.predict <- c(2015, 2025,2030)  #to test
+  #scale <- 100
+  #reg(data.i.c, c(2015, 2025,2030), 100)
+  t.mean <- mean(data$time.period)
+  data <- data |> 
+    mutate(Y=data$obs.value/scale,
+           L = log(Y/(1-Y)),
+           t.prime = time.period - t.mean)
+  
+  lm1 <- lm(L~t.prime, data = data)
+  
+  years.to.predict.prime <- data.frame(t.prime = c(years.to.predict - t.mean))
+  
+  EXP <- exp(predict.lm(lm1, newdata = years.to.predict.prime))
+  
+  return(scale * EXP / ( 1 +EXP))
+  
+}
+# Weighted log regression ----
+# data <- data.i.c
+# scale <- 100
+# wreg(data.i.c, 2030, 100)
+wreg <- function(data, year.to.predict, scale){
+  year.min <- min(data$time.period)
+  t.mean <- mean(data$time.period)
+  
+  #pos <- 1 # to test
+  data.pred <- data |> mutate(wi = (year.to.predict - year.min)/(year.to.predict - time.period),
+                              Y = data$obs.value / scale,
+                              L = log(Y / (1 - Y)),
+                              t.prime = time.period - t.mean)
+  lm1 <- lm(L ~ t.prime, data = data.pred)
+  W <- sum(data.pred$wi)
+  w <- sum(data.pred$wi * data.pred$t.prime * data.pred$L)
+  x <- sum(data.pred$wi * data.pred$t.prime) * sum(data.pred$wi* data.pred$L) / W
+  y <- sum(data.pred$wi * data.pred$t.prime^2)
+  z <- sum(data.pred$wi * data.pred$t.prime)^2 / W
+  
+  r1 <- (w - x) / (y - z)
+  
+  EXP <- exp(lm1$coefficients[[1]] + r1 * (year.to.predict - t.mean))
+  
+  return(scale * EXP / ( 1 + EXP))
+}
+
+
+# CAGR ----
+# data <- data.i.c
+# year.to.predict <- 2025
+# scale <- 100
+# cagr(data, 2030, scale)
+cagr <- function(data, year.to.predict, scale){
+  data <- data |> arrange(time.period) |> 
+    mutate(Y = obs.value/scale, #scaling 
+           obs.value.log = log(Y / (1 - Y)), #logit
+           time.difference = time.period - lag(time.period),
+           cagr = (obs.value.log - lag(obs.value.log)) / time.difference)
+
+  #mean cagr across periods
+  mcagr <- mean(data$cagr, na.rm = TRUE)
+  
+  if (is.nan(mcagr) | is.infinite(mcagr)){
+    #there are cases where
+    # - the value has been constant (SP_ACS_BSRVH2O, KWT) and there is no slope and no mcagr
+    # - The are values of 1, which return an Infinite logit value and no cagr can be calculated
+    #in these cases, we return the latest value of the observation
+    return(tail(data$obs.value, n = 1))
+  }
+  
+  #prediction of logit value
+  EXP <- exp(data$obs.value.log[nrow(data)] + mcagr * (year.to.predict - data$time.period[nrow(data)]))
+  
+  #return inverse logit
+  return(scale * EXP / (1 + EXP))
 }
 
 
